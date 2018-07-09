b2d787ec3058f9ddeea04cfa31d7d3fbb27356cf
Howard Feger <hfeger4@gmail.com>
2018-07-08 17:36:46 -0700

    add posts scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: true, presence: true
+  has_many :posts
+
+  def to_s
+    username
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create, :destroy]
 
+  root 'posts#index'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#create'
