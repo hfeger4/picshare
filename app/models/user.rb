8e0ea534542401f84d793e0888da75d611f9d9f8
Howard Feger <hfeger4@gmail.com>
2018-07-08 23:33:31 -0700

    add posts as dependent on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: true, presence: true
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   def to_s
     username
